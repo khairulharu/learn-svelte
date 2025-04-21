84fb55b55b6fdbdd2a547cbf6be5309e2115f4a0
khairulharu <khairulaswad719@gmail.com>
2025-04-21 19:54:22 +0800

    learn css query selectro

--- src/lib/EditTodo.svelte
+++ src/lib/EditTodo.svelte
@@ -8,3 +8,24 @@
 
 {id} : <input type="text" id={id} bind:value={name}>
 <button onclick={save}>Save</button>
+
+<style>
+     button {
+          background-color: brown;
+          border: none;
+          color: white;
+          padding: 10px 20px;
+          text-align: center;
+          text-decoration: none;
+          display: inline-block;
+          font-size: 14px;
+          margin: 4px 2px;
+          cursor: progress;
+          border-radius: 5px;
+          transition: background-color 0.3s ease;
+     }
+
+     button:hover {
+          background-color: blueviolet;
+     }
+</style>
