82dfd0983bd26647216b7bc7614e27540f4b02ba
khairulharu <khairulaswad719@gmail.com>
2025-05-10 09:57:01 +0800

    learn after doing another thing

--- src/lib/Time.svelte
+++ src/lib/Time.svelte
@@ -1,17 +1,16 @@
 <script>
-     const time = (node) => {
+     const time = (node, interval) => {
           const text = node.textContent
 
           $effect(() => {
                const id = setInterval(() => {
-                    node.textContent = text + ' ' + new Date().toLocaleString()
-               }, 1000)
+                    node.textContent = text + ' ' + new Date().getTime()
+               }, interval)
 
                return () => clearInterval(id)
           })
      }
 </script>
 
-<h1 use:time>Title</h1>
-
-<p use:time>Hello World</p>
+<h1 use:time={1000}>Title</h1>
+<p use:time={5000}>Hello World</p>
--- src/time.js
+++ src/time.js
@@ -1,5 +1,6 @@
 import { mount } from "svelte";
 import Time from "./lib/Time.svelte";
+import "./app.css"
 
 const App = mount(Time, {
      target: document.getElementById("app")
