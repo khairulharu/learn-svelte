954b0e48d59723f79eb38ce81946444962e7906e
khairulharu <khairulaswad719@gmail.com>
2025-05-14 12:49:09 +0800

    learn svelte on mount

--- src/lib/Blog.svelte
+++ src/lib/Blog.svelte
@@ -1,12 +1,24 @@
 <script>
+  import { onMount } from "svelte";
   import Layout from "./Layout.svelte";
 
+  const blog = {
+     title: "",
+     description: ""
+  }
 
+  let blogs = $state([blog])
+
+  onMount(async () => {
+     const response = await fetch("/blog.json")
+     blogs = await response.json()
+  })
 </script>
 
-<Layout title="Halo this is my blgo" footer="Bad result is not mean youre failed">
-     {#snippet content()}
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem facere neque voluptatibus? Temporibus quisquam aliquid omnis quam, eaque veniam officia incidunt illo, expedita, maxime nulla ipsam necessitatibus consequuntur fuga labore.</p>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam quisquam, quas repellendus similique deserunt vel. Ad quia, eum tempora doloribus eos voluptatum repudiandae veniam praesentium quos tempore reprehenderit obcaecati hic.</p>
-     {/snippet}
-</Layout>
+{#each blogs as blog, i (i)}
+     <Layout title={blog.title} footer="Bad result is not mean youre failed">
+          {#snippet content()}
+               {blog.description}
+          {/snippet}
+     </Layout>
+{/each}
